2b78e35f6182eae7d6e5a0b502327c33ea6d9157
yamato5810 <igarashi-yamato-by@ynu.jp>
2023-08-07 21:07:36 +0900

    third commit

--- code/scatter_plot.R
+++ code/scatter_plot.R
@@ -1,1 +1,12 @@
 df_raw <- readr::read_csv("data/owid-co2-data.csv")
+
+dplyr::glimpse(df_raw)
+
+df <- df_raw |>
+  dplyr::select(
+    country:gdp,
+    co2_per_capita
+    ) |>
+  dplyr::filter(
+    year == 2015
+  )
