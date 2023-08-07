ae96813e52ec701097c381727eaa13d7d80f5dfa
hirkame <vjtsx.45afv@gmail.com>
2023-08-07 21:20:42 +0900

    Update wrangling code

--- code/scatter_plot.R
+++ code/scatter_plot.R
@@ -1,12 +1,35 @@
 df_raw <- readr::read_csv("data/owid-co2-data.csv")
 
-dplyr::glimpse(df_raw)
+df_continent <- readr::read_csv("data/all.csv")
+
+
+# dplyr::glimpse(df_raw)
 
 df <- df_raw |>
   dplyr::select(
     country:gdp,
     co2_per_capita
-    ) |>
+  ) |>
   dplyr::filter(
-    year == 2015
+    year == 2015 & 
+      !is.na(gdp)
+  ) |> 
+  dplyr::mutate(
+    gdp_per_capita = gdp / population
   )
+
+df_continent_id <- df_continent |> 
+  dplyr::select(
+    iso_code = `alpha-3`, 
+    region
+  )
+
+df <- df |> 
+  dplyr::left_join(
+    df_continent_id, 
+    by = "iso_code"
+  )
+
+
+
+
